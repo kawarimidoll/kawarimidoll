ffaae33c34063887c8dc0bf34bf4b78e86760bac
kawarimidoll <kawarimidoll+git@gmail.com>
2021-07-29 16:46:36 +0900

    update dependencies

--- deps.ts
+++ deps.ts
@@ -1,11 +1,11 @@
-import ky from "https://cdn.skypack.dev/ky?dts";
+import ky from "https://cdn.skypack.dev/ky@0.28.5?dts";
 import {
   deserializeFeed,
   Feed,
   FeedType,
   RSS1,
   RSS2,
-} from "https://deno.land/x/rss@0.3.6/mod.ts";
+} from "https://deno.land/x/rss@0.4.0/mod.ts";
 import { download } from "https://deno.land/x/download@v1.0.1/mod.ts";
 
 export { deserializeFeed, download, FeedType, ky };
