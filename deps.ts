4274406b886d4e1c9d50a47c758dd692edce3318
GitHub <noreply@github.com>
2021-10-03 00:15:50 +0000

    update deno dependencies

--- deps.ts
+++ deps.ts
@@ -5,7 +5,7 @@ import {
   FeedType,
   RSS1,
   RSS2,
-} from "https://deno.land/x/rss@0.4.0/mod.ts";
+} from "https://deno.land/x/rss@0.5.3/mod.ts";
 import { download } from "https://deno.land/x/download@v1.0.1/mod.ts";
 
 export { deserializeFeed, download, FeedType, ky };
