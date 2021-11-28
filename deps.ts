7932db2a482f4902e7067937431ecdc74331ebce
GitHub <noreply@github.com>
2021-11-28 00:17:11 +0000

    update deno dependencies

--- deps.ts
+++ deps.ts
@@ -1,5 +1,5 @@
 import ky from "https://cdn.skypack.dev/ky@0.28.5?dts";
-import { parseFeed } from "https://deno.land/x/rss@0.5.3/mod.ts";
+import { parseFeed } from "https://deno.land/x/rss@0.5.4/mod.ts";
 import { download } from "https://deno.land/x/download@v1.0.1/mod.ts";
 
 export { download, ky, parseFeed };
